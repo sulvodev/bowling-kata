615ecb048542ce0e519202d0807d795fff0dccd6
Guillermo Peralta Scura <gperaltascura@gmail.com>
2019-03-01 13:46:09 -0300

    Add spare implementation

--- src/main/game.js
+++ src/main/game.js
@@ -1,10 +1,19 @@
 class Game {
   constructor() {
     this.score = 0;
+    this.try = 0;
   }
 
   trackRoll(pinsDown) {
-    if (pinsDown !== 10) this.score += pinsDown;
+    this.try += 1;
+    const newScore = this.score + pinsDown;
+    const strike = this.try === 1 && pinsDown === 10;
+    const spare = this.try === 2 && newScore === 10;
+    if (spare) {
+      this.score = 0;
+    } else if (!strike) { 
+      this.score = newScore
+    }
   }
 
   getScore() {
