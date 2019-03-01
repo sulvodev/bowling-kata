d195069093be42955e5836606608f03da3d03242
Marcelo Franco Filipe Alves <marcelo.franco.alves@gmail.com>
2019-03-01 14:09:01 -0300

    Refact implementation

--- src/main/game.js
+++ src/main/game.js
@@ -1,24 +1,16 @@
 class Game {
   constructor() {
-    this.score = 0;
-    this.try = 0;
+    this.rolls = []
   }
 
   trackRoll(pinsDown) {
-    this.try += 1;
-    const newScore = this.score + pinsDown;
-    const strike = this.try === 1 && pinsDown === 10;
-    const spare = this.try === 2 && newScore === 10;
-    if (spare) {
-      this.score = 0;
-    } else if (!strike) { 
-      this.score = newScore
-    }
+    this.rolls.push({ pinsDown })
   }
 
   getScore() {
-    return this.score;
+    const score = this.rolls.reduce((accum, roll) => accum + roll.pinsDown, 0)
+    return score !== 10 ? score : 0
   }
 }
 
-export default Game;
+export default Game
--- src/tests/game.spec.js
+++ src/tests/game.spec.js
@@ -28,7 +28,6 @@ describe('Testing score traking bowling app - ', () => {
   test('If we have a Spare the score should be 0', () => {
     const game = new Game();
     game.trackRoll(6);
-    expect(game.getScore()).toEqual(6);
     game.trackRoll(4);
     expect(game.getScore()).toEqual(0);
   })
