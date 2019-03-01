e41df32c5293bcb21c8dee086cf18c37666fda9f
David Machado <david.hector.machado@gmail.com>
2019-03-01 12:56:05 -0300

    Remove useless test

--- src/tests/game.spec.js
+++ src/tests/game.spec.js
@@ -1,10 +1,6 @@
 import Game from '../main/game';
 
 describe('Testing score traking bowling app - ', () => {
-  test('can create a game', () => {
-    const game = new Game();
-  })
-
   test('can track one roll', () => {
     const game = new Game();
 
