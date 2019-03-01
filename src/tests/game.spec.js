fda5dca6a465c7a67642388143099ba5189692b7
Vitor Ferrari <vitor@sulvo.com>
2019-03-01 13:08:15 -0300

    testing the second try

--- src/tests/game.spec.js
+++ src/tests/game.spec.js
@@ -1,12 +1,6 @@
 import Game from '../main/game';
 
 describe('Testing score traking bowling app - ', () => {
-  test('can track one roll', () => {
-    const game = new Game();
-
-    game.trackRoll(5);
-  })
-
   test('can get the score of a game', () => {
     const game = new Game();
 
@@ -14,4 +8,17 @@ describe('Testing score traking bowling app - ', () => {
 
     expect(game.getScore()).toEqual(5);
   })
+
+  test('insert the second try into the game', () => {
+    const game = new Game();
+
+    game.trackRoll(5); // first try
+
+    game.trackRoll(3); // second try
+
+    expect(game.getScore()).toEqual(8);
+  })
+
+
+
 })
