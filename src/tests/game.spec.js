2d400a779065815a28c65ac01e84bc3d3209238c
David Machado <david.hector.machado@gmail.com>
2019-03-01 12:50:34 -0300

    new test - can track one roll

--- src/tests/game.spec.js
+++ src/tests/game.spec.js
@@ -4,4 +4,10 @@ describe('Testing score traking bowling app - ', () => {
   test('can create a game', () => {
     const game = new Game();
   })
+
+  test('can track one roll', () => {
+    const game = new Game();
+
+    game.trackRoll(5);
+  })
 })
