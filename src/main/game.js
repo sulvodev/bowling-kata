4bd6b654c353e4487a01e2f6d1c6e7f42ffbd597
Vitor Ferrari <vitor@sulvo.com>
2019-03-01 13:13:28 -0300

    testing  the second try

--- src/main/game.js
+++ src/main/game.js
@@ -1,10 +1,14 @@
 class Game {
-  trackRoll(pinsDown) {
+  constructor() {
+    this.score = 0;
+  }
 
+  trackRoll(pinsDown) {
+    this.score += pinsDown;
   }
 
   getScore() { 
-    return 5;
+    return this.score;
   }
 }
 
