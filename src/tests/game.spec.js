b8e1a029f4cbfcbaa333bd97a5da760b9a8bb207
Guillermo Peralta Scura <gperaltascura@gmail.com>
2019-03-13 12:59:18 -0300

    Test for two strikes in a row

--- src/tests/game.spec.js
+++ src/tests/game.spec.js
@@ -1,9 +1,13 @@
 import Game from '../main/game';
 
 describe('Testing score traking bowling app - ', () => {
-  test('insert the second try into the game', () => {
-    const game = new Game();
+  let game;
 
+  beforeEach(() => {
+    game = new Game()
+  })
+
+  test('insert the second try into the game', () => {
     game.trackRoll(5); // first try
     game.trackRoll(3); // second try
 
@@ -11,21 +15,17 @@ describe('Testing score traking bowling app - ', () => {
   })
 
   test('if Strike happens on the first try the score should be 0', () => {
-    const game = new Game();
-
     game.trackRoll(10);
     expect(game.getScore()).toEqual(0);
   })
 
   test('If we have a Spare the score should be 0', () => {
-    const game = new Game();
     game.trackRoll(6);
     game.trackRoll(4);
     expect(game.getScore()).toEqual(0);
   })
 
   test('if a Spare was made on the first frame and the next try is rolled, it should show the final score ', () => {
-    const game = new Game();
     game.trackRoll(6);
     game.trackRoll(4);
 
@@ -36,11 +36,18 @@ describe('Testing score traking bowling app - ', () => {
   })
 
   test('if a Strike was made on the first frame and the next frame is rolled, it should show the right score', () => {
-    const game = new Game();
     game.trackRoll(10); // + plus the next two rolls
 
     game.trackRoll(3);
     game.trackRoll(2);
     expect(game.getScore()).toEqual(20);
+  })
+
+  test('After two strikes in a row it should show the correct score', () => {
+    game.trackRoll(10);
+    game.trackRoll(10);
+    game.trackRoll(5);
+    game.trackRoll(3);
+    expect(game.getScore()).toEqual(51);
   })
 })
